d97c4129267ca0f73d2b7936679fdef7296b5ac1
humanlee1011 <janghuman@163.com>
2017-02-17 19:24:29 +0800

    debug the homework ajax

--- class4/Homework/static/Homework/js/homework_index.js
+++ class4/Homework/static/Homework/js/homework_index.js
@@ -8,14 +8,12 @@ var uploadfile = function() {
 		// 	$("#hint").show().text('请输入正确格式：学号_姓名.后缀');
 		// 	return　false;
 		// }
-		var formdata = new FormData($('#homework_form')[0]);
-		formdata.append('course_name', $('#course_name').html());
-		formdata.append('homework_name', $('#homework_name').html());
-		// formdata.set('homework_file', input.files[0]);
+		var formdata = new FormData($(this)[0]);
+		formdata.append('course_name', $(this).parent().find('#course_name').html());
+		formdata.append('homework_name', $(this).parent().find('#homework_name').html());
 		$.ajax({
 			method: "POST",
 			url: "upload_homework",
-			//data: $('#homework_form').serialize(),
 			data: formdata,
 			processData:false,
  			contentType:false,
@@ -24,7 +22,7 @@ var uploadfile = function() {
 			},
 			success: function(data) {
 				if (data["success"])
-					$('#hint').html('提交成功');
+					$('.hint').html('提交成功');
 			}
 		});
 		return false;
